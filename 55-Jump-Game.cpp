628ff963bf0600654f89868e251e2d78e162e120
sivaharish51 <sivaharish4251@gmail.com>
2024-12-13 11:09:58 +0530

    [55] [Time Beats: 100.00%] [Memory Beats: 30.00%] - LeetPush

--- 55-Jump-Game.cpp
+++ 55-Jump-Game.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-     int n=nums.size()-1;
-     for(int i=n-1;i>=0;i--){
-        int d=i+nums[i];
-        if(d>=n){
-            n=i;
-        }
-     }   
-     return n==0;
+        int n=nums.size()-1;
+        int d=0;
+        for(int i=n-1;i>=0;i--){
+            d=i+nums[i];
+            if(d>=n){
+                n=i;
+            }
+        }   
+        return n==0; 
     }
 };
