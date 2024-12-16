907c91006baf810ca2fa3dccd7db3c2bcd303f5d
sivaharish51 <sivaharish4251@gmail.com>
2024-12-16 11:25:56 +0530

    [1832] [Time Beats: 100.00%] [Memory Beats: 34.96%] - LeetPush

--- 1832-Check-if-the-Sentence-Is-Pangram.cpp
+++ 1832-Check-if-the-Sentence-Is-Pangram.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
-    bool checkIfPangram(string sentence) {
-        int n=sentence.size();
-        if(n<26){
-            return false;
-        }    
+    bool checkIfPangram(string sentence) {    
         set<char> st(sentence.begin(),sentence.end());
         return st.size()==26;
-
     }
 };
