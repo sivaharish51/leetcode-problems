1f5436283f5cd9c0065347c51cd673b893ac3410
Sivaharish V <sivaharish4251@gmail.com>
2025-03-08 09:39:14 +0530

    Time: 0 ms (100%), Space: 40.4 MB (92.59%) - LeetHub

---
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long flowerGame(long n, long m) {
+        // int c=0;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(i%2==1){
+        //             if(j%2==0){
+        //                 c++;
+        //             }
+        //         }
+        //         else{
+        //             if(j%2==1){
+        //                 c++;
+        //             }
+        //         }
+        //     }
+        // }
+        return (m*n)/2;
+    }
+}
