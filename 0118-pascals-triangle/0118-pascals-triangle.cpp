de40366f73f6fa1153d3cd760b3acb039b9f2bd7
Sivaharish V <sivaharish4251@gmail.com>
2025-05-29 15:06:04 +0530

    Time: 0 ms (100%), Space: 9.8 MB (12.39%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -3,12 +3,12 @@ public:
     vector<vector<int>> generate(int n) {
         vector<vector<int>> a;
         for(int i=0;i<n;i++){
-            vector<int> a1(i+1,1);
-            for(int j=1;j<i;j++){
-                //if(j==0 || j==i) a1[j]=1;
-                //else{
-                    a1[j]=a[i-1][j-1]+a[i-1][j];
-                //}
+            vector<int> a1;
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i) a1.push_back(1);
+                else{
+                    a1.push_back(a[i-1][j-1]+a[i-1][j]);
+                }
             }
             a.push_back(a1);
         }
