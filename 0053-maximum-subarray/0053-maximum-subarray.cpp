70614e05ba323632f7c812913065dcb0fe22f25a
Sivaharish V <sivaharish4251@gmail.com>
2025-06-23 10:00:59 +0530

    Time: 0 ms (100%), Space: 71.7 MB (80.73%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,10 +5,7 @@ public:
         int s=nums[0];
         int a=nums[0];
         for(int i=1;i<n;i++){
-            if(s<0){
-                s=0;
-            }
-            s+=nums[i];
+            s=max(nums[i],s+nums[i]);
             a=max(a,s);
         }
         return a;
