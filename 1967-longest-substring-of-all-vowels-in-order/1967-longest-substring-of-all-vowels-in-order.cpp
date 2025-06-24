0b10a91dcb641c92c7e348eb9c3aabc6245d0096
Sivaharish V <sivaharish4251@gmail.com>
2025-06-24 14:05:09 +0530

    Time: 12 ms (59.52%), Space: 29.4 MB (43.2%) - LeetHub

--- 1967-longest-substring-of-all-vowels-in-order/1967-longest-substring-of-all-vowels-in-order.cpp
+++ 1967-longest-substring-of-all-vowels-in-order/1967-longest-substring-of-all-vowels-in-order.cpp
@@ -2,22 +2,38 @@ class Solution {
 public:
     int longestBeautifulSubstring(string w) {
         int n=w.size();
-        int ans=0,i=0;
-        while(i<n){
-            if(w[i]!='a'){
-                i++;
+        // int ans=0,i=0;
+        // while(i<n){
+        //     if(w[i]!='a'){
+        //         i++;
+        //     }
+        //     int j=i;
+        //     int c=1;
+        //     while(j+1<n && w[j]<=w[j+1]){
+        //         if(w[j]<w[j+1]) c++;
+        //         j++;
+        //     }
+        //     if(c==5){
+        //         ans=max(ans,j-i+1);
+        //     }
+        //     i=j+1;
+        // }
+        // return ans;
+        int c=1,ch=1,a=0;
+        for(int i=0;i<n;i++){
+            if(w[i]==w[i+1]){
+                c++;
             }
-            int j=i;
-            int c=1;
-            while(j+1<n && w[j]<=w[j+1]){
-                if(w[j]<w[j+1]) c++;
-                j++;
+            else if(w[i]<w[i+1]){
+                c++;
+                ch++;
             }
-            if(c==5){
-                ans=max(ans,j-i+1);
+            else{
+                c=1;
+                ch=1;
             }
-            i=j+1;
+            if(ch==5) a=max(a,c);
         }
-        return ans;
+        return a;
     }
 };
