4791a93b0bd8dedf2e6765d56effabf5a9d8617b
Sivaharish V <sivaharish4251@gmail.com>
2025-07-09 20:33:52 +0530

    Time: 787 ms (36.43%), Space: 11.7 MB (36.09%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool search(int i,int j,vector<vector<char>>& board, string word,int n){
+        if(n==word.size()) return true;
+        if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]!=word[n]) return false;
+        char ch=board[i][j];
+        board[i][j]='*';
+        bool l=search(i,j-1,board,word,n+1);
+        bool r=search(i,j+1,board,word,n+1);
+        bool t=search(i-1,j,board,word,n+1);
+        bool b=search(i+1,j,board,word,n+1);
+        board[i][j]=ch;
+        return l||r||t||b;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int n=board.size();
+        int m=board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word[0]){
+                    bool f=search(i,j,board,word,0);
+                    if(f) return true;
+                }
+            }
+        }
+        return false;
+    }
+};
