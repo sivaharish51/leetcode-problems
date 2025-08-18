558e15310dad76f471334755a65d655d09baa5cf
Sivaharish V <sivaharish4251@gmail.com>
2025-08-18 09:44:55 +0530

    Time: 0 ms (100%), Space: 9.1 MB (34.91%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int l=1;
+        int r=n;
+        while(l<=r){
+            long double mid=l+(r-l)/2;
+            long double v=(mid*mid)/2+(mid/2);
+            if(v>n){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return r;
+    }
+};
