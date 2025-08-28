79bc0109716b651dfb067f8c7db77cbed6a3e8d5
Sivaharish V <sivaharish4251@gmail.com>
2025-08-28 13:20:33 +0530

    Time: 0 ms (100%), Space: 13.4 MB (42.81%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,14 +3,19 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int r=matrix.size();
         int c=matrix[0].size();
+        int ind=-1;
         for(int i=0;i<r;i++){
             if(matrix[i][0]<=target && matrix[i][c-1]>=target){
-                for(int j=0;j<c;j++){
-                    if(matrix[i][j]==target){
-                        return true;
-                    }
-                }
+                ind=i;
+                break;
             }
+        }
+        if(ind!=-1){
+        for(int j=0;j<c;j++){
+            if(matrix[ind][j]==target){
+                return true;
+            }
+         }
         }
         return false;
     }
